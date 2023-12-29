039e926704c2a5a270e3d702646df06f7b323942
ZiadSalah2003 <ziadsalah2003@gmail.com>
2023-12-29 18:59:14 +0200

    Add solution for Two Sum problem

---
+++ Easy/Two Sum.cpp
@@ -0,0 +1,29 @@
+// Problem-Link : https://leetcode.com/problems/two-sum/
+// Problem-ID : 1
+// Problem Name: Two Sum
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = i+1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)return {i, j};
+			}
+		}
+		return {};
+    }
+};
