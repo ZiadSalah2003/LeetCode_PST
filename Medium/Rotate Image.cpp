99112d19092c06d68df130a372e6b34162345bce
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-02 01:24:18 +0200

    Add solution for rotating an image

---
+++ Medium/Rotate Image.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/rotate-image/
+// Problem-ID : 48
+// Problem Name: Rotate Image
+// Verdict: AC
+ #include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[j][i], matrix[i][j]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
