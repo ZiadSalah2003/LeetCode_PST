80642744492568ec637b7dadd42ce99a3a34b7a0
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-07 04:10:45 +0200

    Add solution for Array Partition problem

---
+++ Easy/Array Partition.cpp
@@ -0,0 +1,28 @@
+// Problem-Link : https://leetcode.com/problems/array-partition/
+// Problem-ID : 561
+// Problem Name: Array Partition
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum=0;
+        for (int i = 0; i < nums.size()-1; i+=2)
+        {
+            sum+=min(nums[i],nums[i+1]);
+        }
+        return sum;
+    }
+};
