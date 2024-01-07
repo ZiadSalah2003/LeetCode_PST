7fc85df155c441577a7d50058a719e1c86d3a062
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-07 03:40:07 +0200

    Add solution for Remove Duplicates from Sorted Array problem

---
+++ Easy/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Problem-ID : 26
+// Problem Name: Remove Duplicates from Sorted Array
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
