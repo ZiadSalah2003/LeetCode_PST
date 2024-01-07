f21bb6b4c82df3ebae3602dcde742eb5f600eb08
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-07 03:45:01 +0200

    Add solution for Remove Element problem

---
+++ Easy/Remove Element.cpp
@@ -0,0 +1,29 @@
+// Problem-Link : https://leetcode.com/problems/remove-element/
+// Problem-ID : 27
+// Problem Name: Remove Element
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};
