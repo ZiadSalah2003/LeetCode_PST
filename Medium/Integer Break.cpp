6b75b7cddeba61a922c797e48dc4614d02bbd25d
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-27 19:35:49 +0200

    Add solution for Integer Break problem

---
+++ Medium/Integer Break.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/integer-break/description/
+// Problem-ID : 343
+// Problem Name: Integer Break
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int product = 1;
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+        return product;
+    }
+};
