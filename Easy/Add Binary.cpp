1ba24f40404430ee8083f98bc94508c9700c6f1b
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-31 00:03:58 +0200

    Add Binary solution implementation

---
+++ Easy/Add Binary.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/add-binary/
+// Problem-ID : 67
+// Problem Name: Add Binary
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string sum = "";
+        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        while (i >= 0 || j >= 0 || carry) {
+            if (i >= 0) carry += a[i--] - '0';
+            if (j >= 0) carry += b[j--] - '0';
+            sum = char(carry % 2 + '0') + sum;
+            carry /= 2;
+        }
+        return sum;
+    }
+};
