8ba3d31c3d35fa397f93d006d996e0e212c7421a
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-03-03 19:49:55 +0200

    Add solution for Reverse Vowels of a String problem

---
+++ Easy/Reverse Vowels of a String.cpp
@@ -0,0 +1,33 @@
+// Problem-Link : https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+// Problem-ID : 345
+// Problem Name: Reverse Vowels of a String
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string a;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')a+=s[i];
+        }
+        reverse(all(a));
+        int j=0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')s[i]=a[j++];
+        }
+        return s;
+    }
+};
