604cbd696b925bbbc8ad9b563376ffc150d239c5
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-17 17:46:05 +0200

    Add solution for Convert the Temperature problem

---
+++ Easy/Convert the Temperature.cpp
@@ -0,0 +1,24 @@
+// Problem-Link : https://leetcode.com/problems/convert-the-temperature/description/
+// Problem-ID : 2469
+// Problem Name: Convert the Temperature
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return {kelvin, fahrenheit};
+    }
+};
