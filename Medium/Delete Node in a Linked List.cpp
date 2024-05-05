b3b06bd5718b33873de7788d08386e626685a92b
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-05-06 01:47:40 +0300

    feat: Add solution for Delete Node in a Linked List problem

---
+++ Medium/Delete Node in a Linked List.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/generate-parentheses/
+// Problem-ID : 237
+// Problem Name: Delete Node in a Linked List
+// Verdict: AC
+ #include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
