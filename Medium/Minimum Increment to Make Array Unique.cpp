0eb4fa309d9d8c1f10d2e56daa793023c9b187ae
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-06-16 23:14:50 +0300

    Add solution for Minimum Increment to Make Array Unique problem

---
+++ Medium/Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,30 @@
+// Problem-Link : https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+// Problem-ID : 945
+// Problem Name: Minimum Increment to Make Array Unique
+// Verdict: AC
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define sz(x) (int)(x).size()
+#define Endl "\n"
+#define all(v) v.begin(),v.end()
+#define GG  ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+const ll N = 1e3 + 7, OO = 0x3f3f3f3f;
+const ll M = 2e18 + 10;
+const ll MOD = 1e9;
+const double EPS = 1e-7;
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(all(nums));
+        int ans = 0;
+        for (int i = 1; i < sz(nums); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+};
